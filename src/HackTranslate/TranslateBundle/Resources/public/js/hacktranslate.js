e64f07368d00a684f41c6aafa7b8bd851976ee85
Jon Morgan <jon.morgan1974@gmail.com>
2011-11-20 14:55:47 +0000

    jquery translate method added

--- src/HackTranslate/TranslateBundle/Resources/public/js/hacktranslate.js
+++ src/HackTranslate/TranslateBundle/Resources/public/js/hacktranslate.js
@@ -1,2 +1,35 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
+
+    $('#translate').click(function() {
+        translate('it','en','test');
+    });
+
+    function translate(toLanguage, fromLanguage, textToTranslate) {
+        var p = {};
+	    p.appid = '1DEA59FFA7DC652C652134FF0AC9F7170411CC5C';
+	    p.to = toLanguage;
+	    p.from = fromLanguage;
+	    p.text = textToTranslate;
+
+        $.ajax({
+            url: 'http://api.microsofttranslator.com/V2/Ajax.svc/Translate',
+            data: p,
+            dataType: 'jsonp',
+            jsonp: 'oncomplete',
+            jsonpCallback: 'ajaxTranslateCallback',
+            complete: function(request, status) {
+                alert('complete: '+status);
+            },
+            success: function(data, status) {
+                alert('success: data-'+data+',status-'+status);
+            },
+            error: function(request, status, error) {
+                alert('error: status-'+status+',desc-'+error);
+            }
+        });
+    }
+
+    function ajaxTranslateCallback(response) {
+	    alert('ajaxTranslateCallback('+response+')');
+    }
 });
